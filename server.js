c42c9abbc87ff7e809ef093ad6d24e46cf2b483e
shankha001 <shankhasubhrabiswas@gmail.com>
2020-07-29 14:01:05 +0530

    create and read functionality

--- server.js
+++ server.js
@@ -8,10 +8,41 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost:27017/todolistDB', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model('Item', itemsSchema);
+
+//Create
+app.post('/', function (req, res) {
+  const itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect('/');
+});
+
+//Read
+app.get('/', (req, res) => {
+  Item.find({}, (err, items) => {
+    console.log(items);
+    res.render('list', { todoItems: items });
+  });
+});
+
+app.get('/', function (req, res) {
+  res.render('list');
 });
 
 app.listen(3000, () => console.log('Server Started on Port 3000'));
